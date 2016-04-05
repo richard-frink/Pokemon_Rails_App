8bff0e46295445d5ee4e87476ee7582e69047687
Saurabh Khatiwada <khatiwada.saurabh@gmail.com>
2016-04-05 15:01:31 -0400

    Fixed PokeRails logo to link to different things if logged in. Fixed CSS bug that created extra gaps for the devise divs - flash and notice.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,4 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     trainers_index_path # Or :prefix_to_your_route
   end
-  def after_sign_up_path_for(resource)
-    redirect_to root_path # Or :prefix_to_your_route
-  end
-
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_filter :authenticate_user!
+  #before_filter :authenticate_user!
 
   private
 
   def sign_up_params
-    params.require(:user).permit(:username, :avatar, :starter, :email, :password, :password_confirmation, :money)
+    params.require(:user).permit(:username, :email , :password, :password_confirmation, :avatar, :starter, :money)
   end
 
   def account_update_params
